ee96d6960ba0feb562053915c6ed9959ceff1638
TamerlanKhalilli2024 <161066150+TamerlanKhalilli2024@users.noreply.github.com>
2024-02-23 23:29:29 +0400

    Add files via upload

---
+++ day 2.py
@@ -0,0 +1,39 @@
+"""AZƏRBAYCAN"""
+# print("Bu gün menyu yaratmaq qaydası ilə tanış olacağıq")
+# icki = input("İçkilərdən menyuda hansıların olmasını istəyirsiniz? ")
+# soyuq_qelyanalti = input("Soyuq qeylanaltilardan hansı olacaq? ")
+# salat = input("Məşhur salatlardan hansıları əlavə edəcəksiniz? ")
+# sup = input("Restoranda hansı supları hazırlayacaqsınız? ")
+# esas_yemek = input("Əsas yeməklərin adlarını qeyd edin zəhmət olmasa: ")
+# manqal = input("Manqal şefimizin menyu daxil edəcəkləri yeməklər: ")
+# pizza = input("Pizzalardan hansıları menuya əlavə edəcəksiniz? ")
+#
+# print(f"""Restoran menyusunun yaradılması:
+# Menyudakı içkilər: {icki.capitalize()}
+# Menyudakı soyuq qəlyanaltılar: {soyuq_qelyanalti.capitalize()}
+# Menyudakı salatlar: {salat.capitalize()}
+# Menyudakı suplar: {sup.capitalize()}
+# Menyudakı əsas yeməklər: {esas_yemek.capitalize()}
+# Menyudakı manqal yeməkləri: {manqal.capitalize()}
+# Menyudakı pizzalar: {pizza.capitalize()}
+# """)
+
+"""ENGLİSH"""
+print("Today we will learn how to create a menu")
+drinks = input("What drinks would you like to include in the menu? ")
+cold_appetizers = input("Which cold appetizers will be included? ")
+salads = input("Which famous salads will you add? ")
+soups = input("Which soups will you prepare at the restaurant? ")
+main_courses = input("Please list the names of the main courses: ")
+grill = input("Grill dishes that our chef will include in the menu: ")
+pizzas = input("Which pizzas will you add to the menu? ")
+
+print(f"""Creating the restaurant menu:
+Drinks on the menu: {drinks.capitalize()}
+Cold appetizers on the menu: {cold_appetizers.capitalize()}
+Salads on the menu: {salads.capitalize()}
+Soups on the menu: {soups.capitalize()}
+Main courses on the menu: {main_courses.capitalize()}
+Grill dishes on the menu: {grill.capitalize()}
+Pizzas on the menu: {pizzas.capitalize()}
+""")
